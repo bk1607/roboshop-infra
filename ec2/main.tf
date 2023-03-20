d48192dfcca7f55e239c831c45450a88b86ff498
BALA KISHORE ANUMALA <baluanumala16@gmail.com>
2023-03-20 22:17:17 +0530

    output

--- ec2/main.tf
+++ ec2/main.tf
@@ -7,35 +7,12 @@ data "aws_ami" "ami" {
 resource "aws_instance" "ec2" {
   ami = data.aws_ami.ami.image_id
   instance_type = var.type
-  security_groups = [aws_security_group.sg.id]
+  security_groups = [sg_id]
   tags = {
     Name = var.component
   }
 }
 
-resource "aws_security_group" "sg" {
-  name        = "${var.component}-${var.env}-sg"
-  description = "Allow TLS inbound traffic"
-
-  ingress {
-    description      = "allow all"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.component}-dev-sg"
-  }
-}
 //resource "aws_route53_record" "records" {
 //  zone_id = "Z00815241ZW6NBO5CNYD8"
 //  name    = "${var.component}-${var.env}.devops2023.online"
@@ -49,3 +26,4 @@ variable "type" {}
 variable "env" {
   default = "dev"
 }
+variable "sg_id" {}
--- main.tf
+++ main.tf
@@ -3,5 +3,11 @@ module "ec2" {
   for_each = var.instances
   component = each.value["name"]
   type = each.value["type"]
+  sg_id = module.sg.sg_names
 
+}
+module "sg" {
+  source = "./sg"
+  for_each = var.instances
+  component = each.value["name"]
 }
