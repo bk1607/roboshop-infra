496526508e094dee2361adee083e90a466c58abf
BALA KISHORE ANUMALA <baluanumala16@gmail.com>
2023-03-20 17:50:37 +0530

    instances

--- ec2/main.tf
+++ ec2/main.tf
@@ -6,30 +6,30 @@ resource "aws_instance" "ec2" {
   }
 }
 
-resource "aws_security_group" "allow_tls" {
-  name        = "${var.component}-${var.env}-sg"
-  description = "Allow TLS inbound traffic"
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "${var.component}-dev-sg"
-  }
-}
+//resource "aws_security_group" "allow_tls" {
+//  name        = "${var.component}-${var.env}-sg"
+//  description = "Allow TLS inbound traffic"
+//
+//  ingress {
+//    description      = "TLS from VPC"
+//    from_port        = 0
+//    to_port          = 0
+//    protocol         = "-1"
+//    cidr_blocks      = ["0.0.0.0/0"]
+//  }
+//
+//  egress {
+//    from_port        = 0
+//    to_port          = 0
+//    protocol         = "-1"
+//    cidr_blocks      = ["0.0.0.0/0"]
+//    ipv6_cidr_blocks = ["::/0"]
+//  }
+//
+//  tags = {
+//    Name = "${var.component}-dev-sg"
+//  }
+//}
 //resource "aws_route53_record" "records" {
 //  zone_id = "Z00815241ZW6NBO5CNYD8"
 //  name    = "${var.component}-${var.env}.devops2023.online"
