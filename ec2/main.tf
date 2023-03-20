57d2cce1333ac3b3d7afca2b8caf70717a305bb8
BALA KISHORE ANUMALA <baluanumala16@gmail.com>
2023-03-20 18:12:41 +0530

    instances

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,35 +1,40 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+  owners = ["973714476881"]
+}
+
 resource "aws_instance" "ec2" {
-  ami = "ami-0089b8e98cd95257d"
+  ami = data.aws_ami.ami.image_id
   instance_type = var.type
   tags = {
     Name = var.component
   }
 }
 
-//resource "aws_security_group" "allow_tls" {
-//  name        = "${var.component}-${var.env}-sg"
-//  description = "Allow TLS inbound traffic"
-//
-//  ingress {
-//    description      = "TLS from VPC"
-//    from_port        = 0
-//    to_port          = 0
-//    protocol         = "-1"
-//    cidr_blocks      = ["0.0.0.0/0"]
-//  }
-//
-//  egress {
-//    from_port        = 0
-//    to_port          = 0
-//    protocol         = "-1"
-//    cidr_blocks      = ["0.0.0.0/0"]
-//    ipv6_cidr_blocks = ["::/0"]
-//  }
-//
-//  tags = {
-//    Name = "${var.component}-dev-sg"
-//  }
-//}
+resource "aws_security_group" "allow_tls" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "allow all"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.component}-dev-sg"
+  }
+}
 //resource "aws_route53_record" "records" {
 //  zone_id = "Z00815241ZW6NBO5CNYD8"
 //  name    = "${var.component}-${var.env}.devops2023.online"
